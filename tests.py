c7be4d0ab2e38a51c2160350bc24f2972a3d1dce
Yakishev Rashid <businessmanrussia2017@yandex.ru>
2025-12-05 21:55:51 +0500

    Добавил тест добавления в избранное книги, которая не была добавлена в коллекцию книг

--- tests.py
+++ tests.py
@@ -31,20 +31,20 @@ class TestBooksCollector:
         collection.add_new_book(invalid_len_book)
         assert invalid_len_book not in collection.get_books_genre()
          
-    # 3) Позитивная проверка на метод set_book_genre
+    # 4) Позитивная проверка на метод set_book_genre
           
     def test_set_book_genre_correct_genre_successful(self, collection):
         collection.add_new_book('Пуаро')
         collection.set_book_genre('Пуаро', 'Детективы')
         assert collection.get_book_genre('Пуаро') == 'Детективы'
         
-    # 4) Позитивная проверка на метод get_book_genre
+    # 5) Позитивная проверка на метод get_book_genre
     
     def test_get_book_genre_correct_genre_by_book_title(self, collection):
         collection.books_genre = {'Недоросль': 'Комедии'} 
         assert collection.get_book_genre('Недоросль') == 'Комедии'
         
-    # 5) Позитивная проверка на метод get_books_with_correct_genre
+    # 6) Позитивная проверка на метод get_books_with_correct_genre
     
     def test_get_books_with_specific_genre_get_book_with_correct_genre(self, collection):
         collection.books_genre = {
@@ -55,7 +55,7 @@ class TestBooksCollector:
         }
         assert collection.get_books_with_specific_genre('Комедии') == ['Недоросль', 'Горе от ума']
         
-    # 6) Позитивная проверка на метод get_books_for_chilren
+    # 7) Позитивная проверка на метод get_books_for_chilren
     
     def test_get_books_for_children_successful(self, collection):
         collection.books_genre = {
@@ -65,20 +65,26 @@ class TestBooksCollector:
         }                            
         assert collection.get_books_for_children() == ['Джуманджи', 'Ну, погоди']
         
-    # 7) Негативная проверка на метод get_books_for_children
+    # 8) Негативная проверка на метод get_books_for_children
     
     def test_get_books_for_children_when_book_has_no_genre(self, collection):
         collection.add_new_book('Звездные войны')
         assert collection.get_books_for_children() == []
     
-    # 8) Позитивная проверка на метод add_book_in_favorites
+    # 9) Позитивная проверка на метод add_book_in_favorites
     
     def test_add_book_in_favorites_added_successful(self, collection):
         collection.add_new_book('Властелин колец')
         collection.add_book_in_favorites('Властелин колец')
         assert collection.get_list_of_favorites_books() == ['Властелин колец']
+        
+    # 10) Негативная проверка на метод add_book_in_favorites
+    
+    def test_add_book_in_favorites_not_in_library(self, collection):
+        collection.add_book_in_favorites('Несуществующая книга')
+        assert collection.get_list_of_favorites_books() == []
 
-    # 9) Негативная проверка на метод add_book_in_favorites
+    # 11) Негативная проверка на метод add_book_in_favorites
     
     def test_add_book_in_favorites_not_added_twice(self, collection):
         collection.add_new_book('Властелин колец')
@@ -86,7 +92,7 @@ class TestBooksCollector:
         collection.add_book_in_favorites('Властелин колец')
         assert collection.get_list_of_favorites_books() == ['Властелин колец']
 
-    # 10) Позитивная проверка на метод delete_book_from_favorites
+    # 12) Позитивная проверка на метод delete_book_from_favorites
     
     def test_delete_book_from_favorites_successful_deleted(self, collection):
         collection.add_new_book('Буратино')
@@ -94,7 +100,7 @@ class TestBooksCollector:
         collection.delete_book_from_favorites('Буратино')
         assert collection.get_list_of_favorites_books() == []
         
-    # 11) Позитивная проверка на метод get_list_of_favorites_books
+    # 13) Позитивная проверка на метод get_list_of_favorites_books
     
     def test_get_list_of_favorites_books_successful(self, collection):
         collection.favorites = ['Гарри Поттер', 'Властелин колец', 'Буратино']
